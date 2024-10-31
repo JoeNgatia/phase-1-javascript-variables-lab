1041a8c33013bb78d33d8c709cb8cb2ef5efb7cf
Joe <ngatiajoe2@gmail.com>
2024-10-31 16:54:41 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+const mostProfitableNeighborhood = 'Chelsea';
+const companyCeo = 'John Doe'; // Replace 'John Doe' with the actual CEO name if known.
